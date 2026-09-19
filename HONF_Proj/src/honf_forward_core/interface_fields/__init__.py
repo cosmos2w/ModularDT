ca0951a9447803d2522a2fefaa432db717eee792
Linzheng Wang <58857635+cosmos2w@users.noreply.github.com>
2026-09-19 17:21:32 -0400

    Add exact K6 group-control support index

--- HONF_Proj/src/honf_forward_core/interface_fields/__init__.py
+++ HONF_Proj/src/honf_forward_core/interface_fields/__init__.py
@@ -5,6 +5,17 @@ from .fixed_group_pairwise import FixedGroupPairwiseField
 from .fixed_group_router import FixedGroupQueryRoute, FixedGroupRouter, FixedGroupState
 from .group_control_pairwise import GroupControlPairwiseField
 from .group_control_router import GroupQueryRoute, LowDimensionalGroupRouter, PreparedGroupControl
+from .group_control_support import (
+    GROUP_COUNT,
+    QUERY_MASK_COUNT,
+    QuerySignatureSelection,
+    SixBitSupportIndex,
+    SupportExecutionCounts,
+    build_six_bit_support_index,
+    live_pair_values,
+    pack_six_bit_mask,
+    six_bit_mask,
+)
 from .regional_response import RegionalResponseField
 from .three_term_context import ThreeTermInterfaceContext
 from .types import EncodedInterfaceCase, InterfaceRead, PreparedInterfaceField
@@ -17,11 +28,20 @@ __all__ = [
     "FixedGroupState",
     "GroupControlPairwiseField",
     "GroupQueryRoute",
+    "GROUP_COUNT",
     "InterfaceFieldCore",
     "InterfaceRead",
     "LowDimensionalGroupRouter",
+    "QUERY_MASK_COUNT",
     "PreparedGroupControl",
     "PreparedInterfaceField",
+    "QuerySignatureSelection",
     "RegionalResponseField",
+    "SixBitSupportIndex",
+    "SupportExecutionCounts",
     "ThreeTermInterfaceContext",
+    "build_six_bit_support_index",
+    "live_pair_values",
+    "pack_six_bit_mask",
+    "six_bit_mask",
 ]
